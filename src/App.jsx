05e657cf2976967fc8a4d6a8ab9b248213d67f31
QueAnka <annika.nylin@hotmail.com>
2024-03-01 16:25:45 +0100

    installed tailwind, configured pwa manifest and tested tailwindwith an h1 in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './base.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +7,7 @@ function App() {
   return (
     <>
       <div>
-     
+     <h1 className ="font-bold underline" >Hello world!</h1>
       </div>
       
     </>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+
+export default {
+	content: [
+		"./index.html",
+		"./src/**/*.{js,jsx}",
+	],
+	Theme: {
+		extend: {},
+	},
+	plugins: [],
+}
