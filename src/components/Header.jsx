10f5219c928259a5e5e254ff2d9a69e668ff5ee6
AnsoMalm <annsophie.malmqvist85@hotmail.com>
2024-03-05 16:15:55 +0100

    added new files for header to build it and tailwindconfig.js for media querys

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,22 @@
 
+
+
 const Header = () => {
 
 	return(
-		<>Header
-		</>
+		<div className="w-screen h-[150px] z-10 bg-white fixed drop-shadow-lg">
+			<div className="px-7 flex justify-between items-center w-full h-full">
+				<div className="flex items-center">
+					<h1 className="font-merienda text-3xl sm: text-blue-900">Bildstöd i vardagen</h1>
+				</div>
+				<ul className="hidden md:flex">
+					<li>icon. Till Startsida </li>
+					<li>icon Min Veckovy</li>
+					<li>icon Redigera veckovy</li>
+					<li> icon Admin</li>
+				</ul>
+			</div>
+		</div>
 	)
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
 		"./src/**/*.{js,jsx}",
 	],
 	Theme: {
-		extend: {},
+		extend: {
+			sm: "640px",
+			// => @media (min-with: 320px) { ... }
+
+			md: "768px", 
+			// => @media (min-with: 768px) { ... }
+
+			lg: "1024px",
+			// => @media (min-with: 1024px) { ... }
+
+			fontFamily: {
+				merienda: ['Merienda', 'cursive']
+		},
+			
+		}
 	},
 	plugins: [],
 }
