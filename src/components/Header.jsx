833116d994a6895a56313ae946ceccc5986d2b9b
AnsoMalm <annsophie.malmqvist85@hotmail.com>
2024-03-06 14:50:04 +0100

    added colors for the projekt and fixed media guery for headers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,26 +4,26 @@ import { FaInstagram } from "react-icons/fa";
 const Header = () => {
 
 	return(
-		<header className="w-screen h-[150px] z-10 bg-white fixed drop-shadow-lg">
+		<header className="w-screen h-[150px] z-10 bg-white fixed drop-shadow-md">
 			<div className="sm: grid grid-rows-1 justify-items-center lg:flex justify-around items-center w-full h-full">
 				<div className="flex items-center">
-					<h1 className="font-display text-3xl text-blue-900 sm: text-center lg:text-4xl">Bildstöd i vardagen</h1>
+					<h1 className="font-display text-darkblue text-3xl sm:text-4xl lg:text-4xl">Bildstöd i vardagen</h1>
 				</div>
 				<nav>
 				<ul className="hidden lg:flex"  >
-					<li className="grid justify-items-center"> <MdHomeFilled className='size-7' />Till startsida </li>
-					<li className="grid justify-items-center"><MdCalendarToday className='size-7'/> Min veckovy</li>
-					<li className="grid justify-items-center"><MdEdit className='size-7'/> Redigera veckovy</li>
-					<li className="grid justify-items-center"><MdPerson className='size-7'/> Admin</li>
-				</ul>
+					<li className="grid justify-items-center text-sm text-darkblue "> <MdHomeFilled className='size-7' />Till startsida </li>
+					<li className="grid justify-items-center text-sm text-darkblue"><MdCalendarToday className='size-7'/> Min veckovy</li>
+					<li className="grid justify-items-center text-sm text-darkblue"><MdEdit className='size-7'/> Redigera veckovy</li>
+					<li className="grid justify-items-center text-sm text-darkblue"><MdPerson className='size-7'/> Admin</li>
+				</ul> 
 				</nav>
 				<div className="hidden lg:flex">
 					<div className='flex flex-col'>
-						<p className='p-1'>Kontakta oss: </p>
-						<p className='p-1 pb-3'>support@bildstod.se</p>
+						<h5 className='p-1 font-bold text-darkblue'>Kontakta oss: </h5>
+						<p className='p-1 pb-3 text-darkblue'>support@bildstod.se</p>
 						<div className='flex flex-row'>
-							<p className='p-2 pr-3'><MdFacebook className='size-7'/></p>
-							<p className='p-2'><FaInstagram className='size-7'/></p>
+							<p className='p-2 pr-3 text-darkblue'><MdFacebook className='size-7'/></p>
+							<p className='p-2 text-darkblue'><FaInstagram className='size-7'/></p>
 						</div>
 					</div>
 				</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,35 @@ export default {
 		"./index.html",
 		"./src/**/*.{js,jsx}",
 	],
-	Theme: {
+	theme: {
+		colors: {
+			'lightgray': '#F8F8F4',
+			'darkgray': '#A5A5A5',
+			'white': '#FFFFF9',
+			'darkblue': '#180F4A',
+			'green': '#92D050',
+			'lightblue': '#00B0F0',
+			'brown': "#B67100",
+			'yellow': '#FFFF00',
+			'pink': '#FF99CC',
+			'red': '#FF0000',
+			'black': '#1C1B1F',
+			'transparent': 'transparent'
+		},
 		screens: {
-			sm: "640px",
+			xs: "320px", 
 			// => @media (min-with: 320px) { ... }
+			sm: "640px",
+			// => @media (min-with: 640px) { ... }
 	
 			md: "768px", 
 			// => @media (min-with: 768px) { ... }
 	
 			lg: "1024px",
 			// => @media (min-with: 1024px) { ... }
-		},
+			},
 		
 		extend: {
-			// fontFamily: {
-			// 	display: ['"Merienda"', 'cursive']
-			// },
 		}
 	},
 	plugins: [],
