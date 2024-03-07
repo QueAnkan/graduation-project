bc8f73188b3764ab789286eaa57eb545e8373da7
QueAnka <annika.nylin@hotmail.com>
2024-03-07 13:24:40 +0100

    added height on main

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -11,7 +11,7 @@ const Root = () => {
 
 	<div className="flex flex-col">
 		<Header/>
-		<main className="mt-[150px] mx-auto max-w-5xl">
+		<main className="mt-[150px] mx-auto max-w-5xl h-screen h-svh">
 			<Outlet/>
 			<Footer/>
 		</main>	
