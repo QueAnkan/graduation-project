0cc05cc1879f537429362db0874a7b195fd3255c
AnsoMalm <annsophie.malmqvist85@hotmail.com>
2024-03-19 15:50:21 +0100

    started on responsive design for adminpage, not ready yet

--- src/routes/Admin.jsx
+++ src/routes/Admin.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Button from '../utils/style-generators/buttonGenerator'
 import { putNewImage } from "../utils/api-functions/putNewImage";
+import { MdSearch } from "react-icons/md";
 // import {deleteImage} from '../utils/api-functions/deleteImage'
 
 const Admin = () => {
@@ -38,91 +39,109 @@ const Admin = () => {
 	// }
 
 	return(
-	<section className="m-5">
-			<form>
-			<div className="flex flex-col"> 
-				<h1>Lägga till nya bilder i databasen</h1>
-			</div>
-			<label>
-				<p>Välj fil: </p>
-				<input
-					type="file"
-					accept="image/svg"
-					onChange={(event) => {
-						const file = event.target.files[0]; 
-						if(file) {
-							const reader = new FileReader(); 
-							reader.readAsDataURL(file); 
-							reader.onload = () => {
-								const base64String = reader.result.split(',')[1]; 
-								setImage(base64String)
-							}
-					}
-						}}
-				/>
-			</label>
-			<br />
-			<label>
-				<span>Alternativ text</span>
-				<p>Texten beskriver innehållet i bilden och hjälper personer med synnedsättning att förstå dess betydelse</p>
-				<input
-					type="text"
-					name="alt"
-					placeholder="ex. " 
-					onChange={(event) => setAlt(event.target.value)}/>
-					
-			</label>
-			<br />
-			<label>
-				<span>Titel</span>
-				<p>Skriv den titel som ska vara till bilden</p>
-				<input
-					type="text"
-					name="title"
-					// value={title}
-					placeholder="ex. frukost" 
-					onChange={(event) => setTitle(event.target.value)}/>
-			</label>
+	
+	<div className="flex flex-col w-full md:w-[500px] lg:w-[800px]">
+			<section className="mb-4"> 
+				<h1 className="font-bold mt-5 text-xl mb-6">Lägga till nya bilder i databasen</h1>
+				<label htmlFor="input">
+					<input
+						className="text-md"
+						type="file"
+						accept="image/svg"
+						onChange={(event) => {
+							const file = event.target.files[0]; 
+							if(file) {
+								const reader = new FileReader(); 
+								reader.readAsDataURL(file); 
+								reader.onload = () => {
+									const base64String = reader.result.split(',')[1]; 
+									setImage(base64String)
+								}
+						}
+							}}
+					/>
+				</label>
+			</section>
+			<section className="mb-6">
+				<label htmlFor="text">
+				<span className="font-bold">Alternativ text</span>
+				<p className="text-md w-80 md:w-11/12 lg:w-3/4">Texten beskriver innehållet i bilden och hjälper personer med synnedsättning att förstå dess betydelse</p>
+					<div className="flex items-center border border-darkgray rounded-md px-2 py-2 lg:w-1/2">
+						<input
+							type="text"
+							placeholder="ex. tallrik med fil " 
+							onChange={(event) => setAlt(event.target.value)}
+							className="w-full h-9 text-lg"
+						/>
+					</div>
 
-			<p>Ange om bilden är i färg eller svart/vit</p>
-			<label>
-				<input 
-					type="checkbox"
-					value="color"
-					checked={color === 'color'}
-					onChange={(event) => setColor(event.target.checked ? 'color': '')}
-					className="m-2"
-					/>
-					Färgbild
-			</label>
-			<label>
-				<input 
-					type="checkbox"
-					value="bw"
-					checked={color === 'bw'}
-					onChange={(event) => setColor(event.target.checked ? 'bw' : '')}
-					className="m-2"
-					/>
-					Svart/vit bild
-			</label>
-			<Button onClick={handleSubmit}>Lägg till</Button>
-			{uploadSuccess && <p>Bilden har laddats upp framgångsrikt!</p>}
-			</form>
-			
-		<div className="flex flex-col">
-			<p>För att ta bort en bild, sök i databasen</p>
-			<label>
-				<p>Sök</p>
-				<input 
-					type="text"
-				/>
-			</label>
-			{/* <Button onClick={handleSearch}>Sök</Button> */}
-			<div className="m-2">
-					<p>Sökresultat</p>
-			</div>
-		</div>
-		</section>
+				</label>
+			</section>
+			<section className="mb-6">
+				<label htmlFor="title">
+					<span className="font-bold">Titel</span>
+					<p>Skriv den titel som ska vara till bilden</p>
+						<div className="flex items-center border border-darkgray rounded-md px-2 py-2 lg:w-1/2">
+							<input
+								type="text"
+								placeholder="ex. frukost" 
+								onChange={(event) => setTitle(event.target.value)}
+								className="w-full h-9 text-lg"
+							/>
+						</div> 
+				</label>
+			</section>
+			<section className="mb-6">
+				<label htmlFor="checkbox">
+					<div className="mb-4">
+					<span className="font-bold">Ange om bilden är i färg eller svart/vit</span>
+					</div>
+					<input 
+						type="checkbox"
+						value="color"
+						checked={color === 'color'}
+						onChange={(event) => setColor(event.target.checked ? 'color': '')}
+						className="mb-4"
+						/>
+						Färgbild
+				</label>
+				<label htmlFor="checkbox">
+					<input 
+						type="checkbox"
+						value="bw"
+						checked={color === 'bw'}
+						onChange={(event) => setColor(event.target.checked ? 'bw' : '')}
+						className="m-2"
+						/>
+						Svart/vit bild
+				</label>
+				<br />
+				<div className="my-4">
+					<Button onClick={handleSubmit}>Lägg till</Button>
+					{uploadSuccess && <p>Bilden har laddats upp framgångsrikt!</p>}						
+				</div>
+			</section>
+			<label  htmlFor="search">
+							<p className="font-bold">För att ta bort bild, sök i databasen</p>
+							<div className="flex items-center border border-darkgray rounded-md px-2 py-2 lg:w-1/2"> 
+								<div className="h-8 w-8">
+									<MdSearch size={30}/>
+								</div>
+								<input
+									placeholder="ex. frukost"
+									id="search"
+									type="text" 
+									// value = {searchString}
+									// onChange={handleOnChange}
+									className="w-full h-9 text-lg"
+									/>
+							</div>
+						</label>
+				{/* <Button onClick={handleSearch}>Sök</Button> */}
+				<div className="m-2">
+					<p>bilden som kommer ut</p>
+				</div>
+	</div>
 	)
 }
 
