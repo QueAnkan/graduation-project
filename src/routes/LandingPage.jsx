a84d8d927502c7db13716822053f4f10b57e6787
AnsoMalm <annsophie.malmqvist85@hotmail.com>
2024-03-20 10:08:51 +0100

    change som style i landingpage

--- src/routes/LandingPage.jsx
+++ src/routes/LandingPage.jsx
@@ -12,8 +12,8 @@ const LandingPage = () => {
 							className='rounded-full w-full h-auto max-w-md mx-auto sm:w-full md:w-3/4 lg:w-full'
 							/>
 					<article className='m-2 max-w-md flex-col items-center '>
-					<h1> Välkommen till Bildstöd i vardagen, varje dag är unik! </h1>
-					<h2 className='font-bold mt-2'>Med Bildstöd i vardagen kan du:</h2>
+					<h1 className='font-bold sm:text-lg'> Välkommen till Bildstöd i vardagen, varje dag är unik! </h1>
+					<h2 className='mt-3'>Med Bildstöd i vardagen kan du:</h2>
 					<p>Använd bilder för att illustrera dagens aktiviteter för en tydlig struktur. Du kan klicka på en bild för att visa fler bilder som bryter ner aktiviteten i lättförstådda steg. Perfekt för att förbereda och minska oro. </p>
 					</article>
 				</div>
@@ -23,7 +23,7 @@ const LandingPage = () => {
 						alt="Female adult and a child sitting together and look at a mobilephone"
 						className='rounded-md w-full h-auto max-w-md mx-auto sm:order-last sm:max-w-full md:w-3/4 lg:w-full'/>
 					<article className='max-w-md sm:order-first md:flex justify-center items-center md:items-center mx-auto'>
-						<p className='p-2'>Börja skapa veckoschema med bilder redan nu och få vardagen att bli enklare.Låt barnen vara med och utforska sitt schema. Att göra dem delaktiga hjälper dem att känna sig mer förberedda och trygga.  </p> 
+						<p className='p-2'>Börja skapa veckoschema med bilder redan nu och få vardagen att bli enklare. Låt barnen vara med och utforska sitt schema. Att göra dem delaktiga hjälper dem att känna sig mer förberedda och trygga.  </p> 
 					</article>
 				</div>
 		</section>
