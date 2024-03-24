be9875106185a9a3d5430ba9f0ca9af194c52d8e
QueAnka <annika.nylin@hotmail.com>
2024-03-24 08:54:18 +0100

    added style on delete cards and cleaned up code.

--- src/components/SearchOverlay.jsx
+++ src/components/SearchOverlay.jsx
@@ -87,9 +87,10 @@ const SearchOverlay = ({isSearchOpen, handleCloseSearch, handleImageSelected, vi
 					 	<ul className="max-w-xl p-4 sm:flex flex-cols"> 
 							{matchingImages.length > 0 ? (
 								matchingImages.map((image) => (
-								<li key={image.imageId} onClick={() => handleImageClick(image)} className="max-w-60 bg-lightwhite m-3 rounded-md cursor-pointer"> 
+									<li className="max-w-60 bg-lightwhite m-3 rounded-md cursor-pointer"
+									key={image.imageId} onClick={() => handleImageClick(image)} > 
 									<img src={image.imageUrl} alt={image.alt} />
-									<h3 className="text-center font-bold bg-white">{image.title}</h3>
+										<h3 className="text-center font-bold bg-white">{image.title}</h3>
 								</li>
 
 								))
--- src/routes/Admin.jsx
+++ src/routes/Admin.jsx
@@ -21,7 +21,6 @@ const Admin = () => {
 	const [hasSearched, setHasSearched] =useState(false)
 	const [titleIsDirty, setTitleIsDirty] = useState(false)
 	const [altIsDirty, setAltIsDirty] = useState(false)
-	// const [imageIsDirty, setImageIsDirty] =useState(false)
 
 	const [titleIsValid, titleErrorMessage] = isTitleValid(title)
 	const [altIsValid, altErrorMessage] = isAltValid(alt)
@@ -40,10 +39,6 @@ const Admin = () => {
 	}, [uploadSuccess])
 
 	const handleSubmit = async (event) => {
-
-		
-			
-
 		event.preventDefault(); 
 		try {
 			if (!image){
@@ -63,8 +58,6 @@ const Admin = () => {
 				setColorErrorMessage("Vänligen välj om bilden är i färg eller svartvitt.")
 				return
 			}
-
-
 			await putNewImage(alt, color, image, title);
 			setUploadSuccess(true); 
 		}catch (error) {
@@ -104,129 +97,112 @@ const Admin = () => {
 		
 		<section className="mt-10 flex flex-col w-full md:w-[500px] lg:w-[800px]">
 		<KeepLoggedIn />
-		<LogOut/>
-			<form>
-				<section className="mb-10">
-					<div className="flex flex-col"> 
-						<h1 className="font-bold mt-5 text-xl mb-6" >Lägga till nya bilder i databasen</h1>
-					</div>
-					<label htmlFor="imageFile">
-						<h2 className="font-bold">Välj en fil i jpeg-format: </h2>
-						<input
-							id="imageFile"
-							type="file"
-							accept="image/jpg, img/jpeg"
-							className="text-md block w-full 
-							file:mr-4 file:py-2 file:px-4 file:rounded-md
-							file:border-0 file:text-md
-							file:bg-darkblue file:text-white"
-							onChange={(event) => {
-								const file = event.target.files[0]; 
-								if(file) {
-									const reader = new FileReader(); 
-									reader.readAsDataURL(file); 
-									reader.onload = () => {
-										const base64String = reader.result.split(',')[1]; 
-										setImage(base64String)
-									}
+		<form>
+			<section className="mb-10">
+				<div className="flex flex-col"> 
+					<h1 className="font-bold mt-5 text-xl mb-6" >Lägga till nya bilder i databasen</h1>
+				</div>
+				<label htmlFor="imageFile">
+					<h2 className="font-bold">Välj en fil i jpg/jpeg-format: </h2>
+					<input
+						id="imageFile"
+						type="file"
+						accept="image/jpg, img/jpeg"
+						className="text-md block w-full 
+						file:mr-4 file:py-2 file:px-4 file:rounded-md
+						file:border-0 file:text-md
+						file:bg-darkblue file:text-white"
+						onChange={(event) => {
+							const file = event.target.files[0]; 
+							if(file) {
+								const reader = new FileReader(); 
+								reader.readAsDataURL(file); 
+								reader.onload = () => {
+									const base64String = reader.result.split(',')[1]; 
+									setImage(base64String)
 								}
-							}}
-						/>
-					</label>
-					<div className="h-4">
-						{imageErrorMessage && !image && <div className="text-red">{imageErrorMessage} </div>}
+							}
+						}}
+					/>
+				</label>
+				<div className="h-4">
+					{imageErrorMessage && !image && <div className="text-red">{imageErrorMessage} </div>}
+				</div>
+			</section>
+			<section className="mb-10">
+				<label htmlFor="altText">
+					<h2  className="font-bold">Alternativ text</h2>
+					<p className="text-md w-80 md:w-11/12 lg:w-3/4">Texten beskriver innehållet i bilden och hjälper personer med synnedsättning att förstå dess betydelse</p>
+					<div className="flex items-center border border-darkgray rounded-md px-2 py-2 w-72 lg:w-1/2">
+						<input
+							id="altText"
+							type="text"
+							name="alt"
+							className="w-full h-9 text-base"
+							placeholder="ex. Färgad bild av en tallrik med flingor och ett glas mjölk. max 150 tecken " 
+							max={120}
+							onChange={(event) => setAlt(event.target.value)}
+							onBlur={() => setAltIsDirty(true)}/>
 					</div>
-				</section>
-
-
-
-
-				<section className="mb-10">
-					<label htmlFor="altText">
-						<h2  className="font-bold">Alternativ text</h2>
-						<p className="text-md w-80 md:w-11/12 lg:w-3/4">Texten beskriver innehållet i bilden och hjälper personer med synnedsättning att förstå dess betydelse</p>
-						<div className="flex items-center border border-darkgray rounded-md px-2 py-2 w-72 lg:w-1/2">
-							<input
-								id="altText"
-								type="text"
-								name="alt"
-								className="w-full h-9 text-base"
-								placeholder="ex. Färgad bild av en tallrik med flingor och ett glas mjölk. max 150 tecken " 
-								max={120}
-								onChange={(event) => setAlt(event.target.value)}
-								onBlur={() => setAltIsDirty(true)}/>
-						</div>
-					</label>
-					<div className="h-4 pt-2 text-red"> {altIsDirty ? altErrorMessage  : ''}</div>
-				</section>
-
-
-
-				<section className="mb-10">
-					<label htmlFor="title">
-						<h2 className="font-bold">Titel</h2>
-						<p>Skriv den titel som ska vara till bilden</p>
-						<div className="flex items-center border border-darkgray rounded-md p-2 w-72 lg:w-1/2">
-							<input
-								id="title"
-								type="text"
-								name="title"
-								className="w-full h-9 text-base"
-								placeholder="ex. frukost" 
-								maxLength={35}
-								onChange={(event) => setTitle(event.target.value)}
-								onBlur={() => setTitleIsDirty(true)}/>
-						</div>
-					</label>
-					<div className="h-4 pt-2 text-red"> {titleIsDirty ? titleErrorMessage  : ''}</div>
-				</section>
-
-
-
-				<section className="mb-16">
-						<div className="mb-4">
-							<h2 className="font-bold">Ange om bilden är i färg eller svart/vit</h2>
-						</div>
-					<label htmlFor="colorCheckbox" className="mr-4">
-						<input 
-							id="colorCheckbox"
-							type="checkbox"
-							value="color"
-							className="m-2"
-							checked={color === 'color'}
-							onChange={(event) => setColor(event.target.checked ? 'color': '')}
-							/>
-							Färgbild
-					</label>
-					<label  htmlFor="bwCheckbox">
-						<input 
-						id="bwCheckbox"
+				</label>
+				<div className="h-4 pt-2 text-red"> {altIsDirty ? altErrorMessage  : ''}</div>
+			</section>
+			<section className="mb-10">
+				<label htmlFor="title">
+					<h2 className="font-bold">Titel</h2>
+					<p>Skriv den titel som ska vara till bilden</p>
+					<div className="flex items-center border border-darkgray rounded-md p-2 w-72 lg:w-1/2">
+						<input
+							id="title"
+							type="text"
+							name="title"
+							className="w-full h-9 text-base"
+							placeholder="ex. frukost" 
+							maxLength={35}
+							onChange={(event) => setTitle(event.target.value)}
+							onBlur={() => setTitleIsDirty(true)}/>
+					</div>
+				</label>
+				<div className="h-4 pt-2 text-red"> {titleIsDirty ? titleErrorMessage  : ''}</div>
+			</section>
+			<section className="mb-16">
+					<div className="mb-4">
+						<h2 className="font-bold">Ange om bilden är i färg eller svart/vit</h2>
+					</div>
+				<label htmlFor="colorCheckbox" className="mr-4">
+					<input 
+						id="colorCheckbox"
 						type="checkbox"
-						value="bw"
-						checked={color === 'bw'}
-						onChange={(event) => setColor(event.target.checked ? 'bw' : '')}
+						value="color"
 						className="m-2"
+						checked={color === 'color'}
+						onChange={(event) => setColor(event.target.checked ? 'color': '')}
 						/>
-							Svart/vit bild
-					</label>
-					<div className="h-4">
-						{colorErrorMessage && !color && <div className="text-red ">{colorErrorMessage}</div>}
-						</div>
-					<div className="my-10 mx-auto ">
-						<p className="pb-4">När alla fält är ifyllda kan du lägga till en bild i databasen.</p>
-					<Button 
-						onClick={handleSubmit}
-						>Lägg till</Button>
-					{uploadSuccess && <p>Bilden har laddats upp framgångsrikt!</p>}
+						Färgbild
+				</label>
+				<label  htmlFor="bwCheckbox">
+					<input 
+					id="bwCheckbox"
+					type="checkbox"
+					value="bw"
+					checked={color === 'bw'}
+					onChange={(event) => setColor(event.target.checked ? 'bw' : '')}
+					className="m-2"
+					/>
+						Svart/vit bild
+				</label>
+				<div className="h-4">
+					{colorErrorMessage && !color && <div className="text-red ">{colorErrorMessage}</div>}
 					</div>
-				</section>
-			</form>
-			
-
-
-
-
-
+				<div className="my-10 mx-auto ">
+					<p className="pb-4">När alla fält är ifyllda kan du lägga till en bild i databasen.</p>
+				<Button 
+					onClick={handleSubmit}
+					>Lägg till</Button>
+				{uploadSuccess && <p>Bilden har laddats upp framgångsrikt!</p>}
+				</div>
+			</section>
+		</form>
 		<div className="flex flex-col items-start">
 			<label htmlFor="search" className="w-full">			
 				<h2 className="font-bold">För att ta bort en bild, sök i databasen</h2>
@@ -241,36 +217,38 @@ const Admin = () => {
 						value = {searchString}
 						onChange={handleOnChange}
 						className="w-full h-9 text-base"
-					/>
+						/>
 				</div>
 			</label>
-				<div className="py-6">
-						<Button onClick={handleSearch}>Sök</Button>
-						
-						<p className="mt-4 ">{hasSearched ?"Klicka på den bilden du vill ta bort": ""}</p>
-				</div>
-					<div className="mt-5">
-					 	<ul> 
-							{matchingImages.length > 0 ? (
-								matchingImages.map((image) => (
-								<li 
+			<div className="py-6">
+				<Button disabled={!searchString} onClick={handleSearch}>Sök</Button>
+				
+				<p className="mt-4 ">{hasSearched ?"Klicka på den bilden du vill ta bort": ""}</p>
+			</div>
+			<div className="mt-5">
+				<ul className="max-w-xl p-4 sm:flex flex-cols"> 
+					{matchingImages.length > 0 ? (
+						matchingImages.map((image) => (
+							<li className="max-w-60 bg-lightwhite m-3 rounded-md" 
 								key={image.imageId}>		
-										<h3>{image.title}</h3>
-										<img src={image.imageUrl} alt={image.alt} />
-										<Button 
-											onClick={() => handleDeleteImage(image.imageId)}
-											style="transparent">
-												<p>Ta bort bild </p> 
-												<p><RiDeleteBin6Line size={30} /> </p>
-										</Button>
-								</li>
-								))
-							) : (
-								hasSearched ? "Inga resultat matchade sökningen" : " "
-							)}
-						</ul> 
-					</div>			
+								<img src={image.imageUrl} alt={image.alt} 
+								/>
+								<h3 className="text-center font-bold bg-white">{image.title}</h3>
+								<Button 
+									className="flex items-center pt-2 mx-auto"
+									onClick={() => handleDeleteImage(image.imageId)}
+									style="transparent">
+										<p>Ta bort bild </p> 
+										<p><RiDeleteBin6Line size={30} /> </p>
+								</Button>
+							</li>
+						))
+						) : (hasSearched ? "Inga resultat matchade sökningen" : " ")	
+					}
+				</ul> 
+			</div>			
 		</div>
+<LogOut/>
 		</section>
 	)
 }
