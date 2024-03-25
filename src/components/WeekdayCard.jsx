0aeebcd55d9dc8faae8c7d7c0002ab3cbe111381
QueAnka <annika.nylin@hotmail.com>
2024-03-25 08:49:31 +0100

    moved global function to new file

--- src/components/WeekdayCard.jsx
+++ src/components/WeekdayCard.jsx
@@ -7,16 +7,8 @@ import { moveToFormerDay, moveToNextDay } from "../data/routeToPath";
 import { useVisibilityStatus } from "../utils/VisibleElementProvider";
 import { useEffect, useState } from "react";
 import SearchOverlay from "./SearchOverlay";
+import { saveImagesToLocalStorage, getImagesFromLocalStorage } from "../utils/global-functions/localStorage";
 
-
-const saveImagesToLocalStorage = (key, images) => {
-		localStorage.setItem(key, JSON.stringify(images));
-};
-	
-const getImagesFromLocalStorage = (key) => {
-    const images = localStorage.getItem(key);
-    return images ? JSON.parse(images) : [];
-};
 
 const WeekdayCard = (props) => {
 	const {isVisible} = useVisibilityStatus()
@@ -66,57 +58,50 @@ const WeekdayCard = (props) => {
 
 	const handleOnclick = () => {
 		toggleIsVisible()
-		console.log('click');
 	}
 	
-
 	return( 	
-	<>
-		<article className="mx-auto my-4 w-10/12 text-center lg:my-10" >
-			<h1 className="text-xl font-bold py-4">Planera ditt veckoschema</h1>
-			<p>Här kan du lätt välja bilder för att planera 
-				upp dagens och veckans aktiviteter. På varje 
-				dag kan du lägga till fler bilder. Klickar du på detaljerad vy kan du lägga till fler bilder för varje aktivitet under dagen för att bryta ner den ännu mer. Du kan även ta bort bilder ifall du ångrar ditt val.  
-			</p>
-		</article>
-
-
-		<section className="flex justify-center item-center relative w-10/12 mx-auto my-6 md:mt-10" >
-			<button 
-				onClick={handleFormerDay} 
-				aria-label="Byt veckodag bakåt" 
-				className="  left-[calc(64+30)] top-0 sm:left-12 md:absolute md:left-8 md:top-60"><TfiArrowCircleLeft size={30} />
-			</button>
-			<h2 className="text-2xl mx-10">{props.view}</h2>
-			<button 
-				onClick={handleNextDay} 
-				aria-label="Byt veckodag framåt" 
-				className=" right-[calc(64+30)] top-0 sm:right-12 md:absolute md:right-8 md:top-60"><TfiArrowCircleRight size={30} />
-			</button>
-		</section>
-
-
-		<section className={`${bgColorClassName} flex flex-col rounded-md border border-darkgray w-10/12 mx-auto my-4 px-4 py-10 md:w-fit md:px-16`} >
-			<section className="flex flex-col items-center w-fit mx-auto">	
-				<ImageContainer 
-					images={selectedImages} 	
-					handleImageDelete={handleImageDelete}/>
-			</section>	
-			
-			<span className="mx-auto inline-block mt-20 mb-8 ">
-				{isVisible && <Button onClick={openSearchOverlay}>Lägg till bild</Button>}
-			</span>
-			
-			<span className="mx-auto inline-block">
-				{isVisible && <Button onClick={handleOnclick} >Avsluta redigering</Button>}
-			</span>
-			
-		</section>
-		{isSearchOpen && <SearchOverlay
-							isSearchOpen={isSearchOpen}
-							handleCloseSearch={handleCloseSearch}
-							handleImageSelected={handleImageSelected}/>}
-	</>
+		<>
+			<article className="mx-auto my-4 w-10/12 text-center lg:my-10" >
+				<h1 className="text-xl font-bold py-4">Planera ditt veckoschema</h1>
+				<p>Här kan du lätt välja bilder för att planera upp dagens och veckans aktiviteter. På varje dag kan du lägga till fler bilder. Klickar du på detaljerad vy kan du lägga till fler bilder för varje aktivitet under dagen för att bryta ner den ännu mer. Du kan även ta bort bilder ifall du ångrar ditt val.  
+				</p>
+			</article>
+			<section className="flex justify-center item-center relative w-10/12 mx-auto my-6 md:mt-10" >
+				<button 
+					onClick={handleFormerDay} 
+					aria-label="Byt veckodag bakåt" 
+					className="  left-[calc(64+30)] top-0 sm:left-12 md:absolute md:left-8 md:top-60"><TfiArrowCircleLeft size={30} />
+				</button>
+				<h2 className="text-2xl mx-10">{props.view}</h2>
+				<button 
+					onClick={handleNextDay} 
+					aria-label="Byt veckodag framåt" 
+					className=" right-[calc(64+30)] top-0 sm:right-12 md:absolute md:right-8 md:top-60"><TfiArrowCircleRight size={30} />
+				</button>
+			</section>
+			<section className={`${bgColorClassName} flex flex-col rounded-md border border-darkgray w-10/12 mx-auto my-4 px-4 py-10 md:w-fit md:px-16`} >
+				<section className="flex flex-col items-center w-fit mx-auto">	
+					<ImageContainer 
+						images={selectedImages} 	
+						handleImageDelete={handleImageDelete}/>
+				</section>	
+				
+				<span className="mx-auto inline-block mt-20 mb-8 ">
+					{isVisible && <Button onClick={openSearchOverlay}>Lägg till bild</Button>}
+				</span>
+				
+				<span className="mx-auto inline-block">
+					{isVisible && <Button onClick={handleOnclick} >Avsluta redigering</Button>}
+				</span>
+				
+			</section>
+			{isSearchOpen && 
+				<SearchOverlay
+					isSearchOpen={isSearchOpen}
+					handleCloseSearch={handleCloseSearch}
+					handleImageSelected={handleImageSelected}/>}
+		</>
 	)
 }
 
---
+++ src/utils/global-functions/localStorage.js
@@ -0,0 +1,8 @@
+export const saveImagesToLocalStorage = (key, images) => {
+	localStorage.setItem(key, JSON.stringify(images));
+};
+
+export const getImagesFromLocalStorage = (key) => {
+const images = localStorage.getItem(key);
+return images ? JSON.parse(images) : [];
+};
